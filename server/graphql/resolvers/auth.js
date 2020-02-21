8ebd037d46675932244489839833e3bc829ca1f9
JWaltuch <jenna.waltuch@gmail.com>
2020-02-21 14:22:00 -0500

    wrote login resolver

--- server/graphql/resolvers/auth.js
+++ server/graphql/resolvers/auth.js
@@ -1,4 +1,6 @@
 const User = require('../../database/models/user')
+const jwt = require('jsonwebtoken')
+if (process.env.NODE_ENV !== 'production') require('../secrets')
 
 module.exports = {
   // users: async () => {
@@ -9,20 +11,41 @@ module.exports = {
   //     console.log(error)
   //   }
   // },
-  signUp: async args => {
+  signUp: async ({userInput}) => {
     try {
       let existingUser = await User.findOne({
-        where: {email: args.userInput.email}
+        where: {email: userInput.email}
       })
       if (existingUser) {
         throw new Error('This email is already taken.')
       }
       let newUser = await User.create({
-        username: args.userInput.username,
-        email: args.userInput.email,
-        password: args.userInput.password
+        username: userInput.username,
+        email: userInput.email,
+        password: userInput.password
       })
       return {...newUser, password: null}
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  login: async ({email, password}) => {
+    try {
+      let existingUser = await User.findOne({
+        where: {email: email}
+      })
+      if (!existingUser) {
+        throw new Error('Email or password is incorrect.')
+      } else if (!existingUser.correctPassword(password)) {
+        throw new Error('Email or password is incorrect.')
+      } else {
+        const token = jwt.sign(
+          {userId: existingUser.id},
+          process.env.SESSION_SECRET,
+          {expiresIn: '2h'}
+        )
+        return {userId: existingUser.id, token: token, tokenExpiration: 2}
+      }
     } catch (error) {
       console.log(error)
     }
