d93a6dff5d00c5101b2efe89a0d8c07dd73a10a9
JWaltuch <jenna.waltuch@gmail.com>
2020-03-12 10:59:55 -0400

    feat: wrote auth middleware, added to app routing index

--- server/app.js
+++ server/app.js
@@ -7,9 +7,14 @@ const graphqlHttp = require('express-graphql')
 const graphqlSchema = require('./graphql/schema/index')
 const graphqlResolvers = require('./graphql/resolvers/index')
 
+//graphql middleware import
+const isAuth = require('./graphql/middleware/is-auth')
+
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use(isAuth)
 
 app.use(
   '/graphql',
---
+++ server/graphql/middleware/is-auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get('Authorization')
+  //If the user is not autheticated, should pass info that lets subsequent requests know if can only access certain endpoints
+  if (!authHeader) {
+    req.isAuth = false
+    next()
+  }
+  const token = authHeader.split(' ')[1] // Bearer TOKEN
+  if (!token || token === '') {
+    req.isAuth = false
+    next()
+  }
+  let decodedToken = ''
+  try {
+    decodedToken = jwt.verify(token, process.env.SESSION_SECRET)
+  } catch (error) {
+    req.isAuth = false
+    return next
+  }
+  if (!decodedToken) {
+    req.isAuth = false
+    next()
+  }
+  req.isAuth = true
+  req.userId = decodedToken
+}
