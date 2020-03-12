d21d5fba37a14208ce47699ce315d97027076b98
JWaltuch <jenna.waltuch@gmail.com>
2020-03-12 15:59:31 -0400

    feat: wrote some initial front end routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/* Routes placed here are available to all visitors */}
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        {isLoggedIn && (
+          <Switch>
+            {/* Routes placed here are only available after logging in */}
+            <Route path="/home" component={UserHome} />
+          </Switch>
+        )}
+        {/* Displays our Login component as a fallback */}
+        <Route component={Login} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
